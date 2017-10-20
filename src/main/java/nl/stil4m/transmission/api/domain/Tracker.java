b5f0455a6b6e047a873e895f0db0126910cd68ae
UDD <udelledo@gmail.com>
2017-10-20 23:13:12 +0200

    Refactor for lombok

--- src/main/java/nl/stil4m/transmission/api/domain/Tracker.java
+++ src/main/java/nl/stil4m/transmission/api/domain/Tracker.java
@@ -1,41 +1,12 @@
 package nl.stil4m.transmission.api.domain;
 
+import lombok.Data;
+
+@Data
 public class Tracker {
 
     private String announce;
     private Long id;
     private String scrape;
     private Long tier;
-
-    public String getAnnounce() {
-        return announce;
-    }
-
-    public void setAnnounce(String announce) {
-        this.announce = announce;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getScrape() {
-        return scrape;
-    }
-
-    public void setScrape(String scrape) {
-        this.scrape = scrape;
-    }
-
-    public Long getTier() {
-        return tier;
-    }
-
-    public void setTier(Long tier) {
-        this.tier = tier;
-    }
 }
