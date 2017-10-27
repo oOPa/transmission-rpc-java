834a6cc0529dd0d4c27d5556f70e427450656dd2
udelledo <udelledo@gmail.com>
2017-10-27 09:06:50 +0200

    removed basic authentication

--- src/test/java/nl/stil4m/transmission/Constants.java
+++ src/test/java/nl/stil4m/transmission/Constants.java
@@ -3,7 +3,7 @@ package nl.stil4m.transmission;
 public class Constants {
 
 	public final static String TEST_URL = "http://localhost:1091/transmission/rpc";
-	public static final String TEST_PASSWORD = "t4nt3t3tt";
+	public static final String TEST_PASSWORD = "";
 	public static final String TEST_USER = "admin";
 	public static final String TEST_WHITEHOUSE_NAME = "Scorpion.S04E05.HDTV.x264-SVA[eztv].mkv[eztv]";
 	public static final String TEST_WHITEHOUSE_MAGNET = "magnet:?xt=urn:btih:2cd2e4b591dff8bc00574a90b30481d22c62ffc9&dn=Scorpion.S04E05.HDTV.x264-SVA%5Beztv%5D.mkv%5Beztv%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A80&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969";
